eac2f225ae398b7931e15eff7816aa0898982138
Minhtrong360 <nguyenminhtrong402@gmail.com>
2022-12-15 22:47:35 +0700

    chua biet test thunder

--- server/app.js
+++ server/app.js
@@ -14,6 +14,45 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+
+const { error } = require("console");
+/* DB connection*/
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log(`DB connected ${mongoURI}`))
+  .catch((err) => console.log(err));
+
+// const main = async () => {
+//   await mongoose.connect(mongoURI);
+//   console.log("connect success to ", mongoURI);
+// };
+// main().catch((err) => console.log(err));
+
+// bước 3
+const { sendResponse, AppError } = require("./helpers/utils.js");
+
+// catch 404 and forard to error handler
+app.use((req, res, next) => {
+  const err = new AppError(404, "Not Found", "Bad Request");
+  next(err);
+});
+
+/* Initialize Error Handling */
+app.use((err, req, res, next) => {
+  console.log("ERROR", err);
+  return sendResponse(
+    res,
+    err.statusCode ? err.statusCode : 500,
+    false,
+    null,
+    { message: err.message },
+    err.isOperational ? err.errorType : "Internal Server Error"
+  );
+});
 
 app.use("/", indexRouter);
 
